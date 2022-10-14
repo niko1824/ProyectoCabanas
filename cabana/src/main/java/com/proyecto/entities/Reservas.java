3f5dcb9bf647c580848cde1c2d45e664b8edbfe9
niko1824 <114964772+niko1824@users.noreply.github.com>
2022-10-14 01:24:06 -0500

    cambio category

--- cabana/src/main/java/com/proyecto/entities/Reservas.java
+++ cabana/src/main/java/com/proyecto/entities/Reservas.java
@@ -108,11 +108,5 @@ public class Reservas implements Serializable {
         this.cabin = cabin;
     }
 
-    public List<Message> getMessages() {
-        return messages;
-    }
 
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
 }

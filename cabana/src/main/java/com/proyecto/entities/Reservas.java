909e93f14c7caa84179cfb45b908fb9d3ed86f0b
niko1824 <114964772+niko1824@users.noreply.github.com>
2022-10-13 22:13:30 -0500

    prueba category

--- cabana/src/main/java/com/proyecto/entities/Reservas.java
+++ cabana/src/main/java/com/proyecto/entities/Reservas.java
@@ -30,7 +30,7 @@ public class Reservas implements Serializable {
 
     @ManyToOne
     @JoinColumn(name = "category_id")
-    @JsonIgnoreProperties("reservation")
+    @JsonIgnoreProperties("reservations")
     private Category category;
 
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "cabin")
